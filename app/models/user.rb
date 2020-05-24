670bd1585618eabba4705a060493b88ff3b0e47e
kazutaka-div <kazutaka.gotanda@di-v.co.jp>
2020-05-24 23:03:58 +0900

    wizard

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
   has_one :sns_credential, dependent: :destroy
+  has_one :address, dependent: :destroy
 
   validates :nickname, :birthday, :first_name, :last_name, presence: true
   validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
