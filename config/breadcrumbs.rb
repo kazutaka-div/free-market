756816343be61143edb568afd1786435729f3c7d
kazutaka-div <kazutaka.gotanda@di-v.co.jp>
2020-06-07 15:32:40 +0900

    category-fix

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,9 +2,8 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-crumb :item do
-  show_title = Item.find_by(id: params[:id])
-  link show_title.name
+crumb :item do |item|
+  link item.name
   parent :root
 end
 
@@ -13,8 +12,7 @@ crumb :category do
   parent :root
 end
 
-crumb :category_show do
-  category = Category.find(params[:id])
+crumb :category_show do |category|
   link category.name
   parent :category
 end
